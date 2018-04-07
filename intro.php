ea67d1b3ff66ac6b89fd2940aaa1639982a2d651
zwan2 <2zwan@naver.com>
2018-04-07 19:48:03 +0900

    0.2.2
    업로드용

--- intro.php
+++ intro.php
@@ -7,7 +7,7 @@ include 'top.php';
 	<br/>
 	<h1 class="display-1 text-bold">건대후문</h1>
 	<p class="bd-lead text-justify">18.04.08 베타 서비스 시작<br/>18.04.05 개발 시작</p>
-	<p class="bd-lead text-justify">도움 주신 분들</p>
+	<p class="bd-lead text-justify">도움 주신 분들<br/>뱁새</p>
 	<p class="bd-lead text-justify">문의</br/>zwaninkt@gmail.com</p>
 	<br/>
 </div>
